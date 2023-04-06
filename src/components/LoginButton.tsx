d947ca40e3859a7d59ceb7ea2575383c69db8947
Thomas Olsson <olss.thom@gmail.com>
2023-04-06 12:37:54 +0200

    updates boilerplate

--- src/components/LoginButton.tsx
+++ src/components/LoginButton.tsx
@@ -1,11 +1,11 @@
 import { useSession, signOut, signIn } from "next-auth/react";
 
 export function LoginButton() {
-  const { data: sessionData } = useSession();
+  const { data: session } = useSession();
 
   return (
-    <button onClick={sessionData ? () => void signOut() : () => void signIn()}>
-      {sessionData ? "Sign out" : "Sign in"}
+    <button onClick={session ? () => signOut() : () => signIn()}>
+      {session ? "Sign out" : "Sign in"}
     </button>
   );
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,27 @@
+import { Flex, Group, Text } from "@mantine/core";
+import { LoginButton } from "./LoginButton";
+import Link from "next/link";
+import { useSession } from "next-auth/react";
+
+export default function Navbar() {
+  const { data: session } = useSession();
+
+  return (
+    <nav>
+      <Flex
+        p="md"
+        justify="space-between"
+        align="center"
+        sx={{
+          boxShadow: "0 0 10px rgba(0, 0, 0, 0.1)",
+        }}
+      >
+        <Link href="/">Home</Link>
+        <Group>
+          {session && <Text>{session.user.email}</Text>}
+          <LoginButton />
+        </Group>
+      </Flex>
+    </nav>
+  );
+}
